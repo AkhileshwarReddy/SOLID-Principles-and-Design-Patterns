920e324f7ac04dc2db5719185c8a6aa12430bf20
Akhileshwar Reddy <akhil.vedas@outlook.com>
2021-10-03 20:40:55 +0530

    SOLID Principles - Add Work-Around for Single Responsible Principle

---
+++ SingleResponsibilityPrinciple/Pesistence.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace SingleResponsibilityPrinciple
+{
+    class Pesistence
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if(overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, j.ToString());
+            }
+        }
+    }
+}
---
+++ SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SingleResponsibilityPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var journal = new Journal();
+            journal.AddEntry("I cried today");
+            journal.AddEntry("I ate bug");
+
+            var persistence = new Pesistence();
+            var filename = @"c:/temp/journal.txt";
+            persistence.SaveToFile(journal, filename, true);
+        }
+    }
+}
